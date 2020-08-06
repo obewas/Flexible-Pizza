737a2e455f37250f4a4d467240c0823ee1db1bd3
obewas <obewas1202@gmail.com>
2020-08-06 10:53:39 +0300

    merged branches

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
 //Business Logic
 
-function PizzaOrder(size, flavor, toppings, qty) {
+function PizzaOrder(size, flavor, toppings, qty,customer) {
   this.size = size
   this.flavor = flavor
   this.toppings = toppings
   this.qty = qty
+  this.customer = function(fname, lname, city, estate, contsct, hse){
+    var fname = document.getElementById('fname').value
+    var lname = document.getElementById('lname')
+    var city = document.getElementById('city')
+    var estate = document.getElementById('estate')
+    var contact = document.getElementById('ctc')
+    var hse = document.getElementById('hse')
+  }
+  
 }
 //var newOrder = new PizzaOrder(size,flavor,toppings,qty)
 
-function totalCost(){
-var size = document.getElementsByTagName('size').value
-alert(size)
+
+  function totalCost(costFlavor,costSize, costTops){
+    this.costFlavor = costFlavor
+    this.costSize = costSize
+    this.costTops = costTops
+var size = document.querySelectorAll("input[name=size]:checked")[0].value
 costSize = 0
 if (size === 'small'){
   costSize += 400
@@ -22,22 +34,20 @@ if (size === 'small'){
   costSize += 700;
 }
 
-var select = document.getElementById('flavor').value;
-cost = 0;
-alert(select);
-if (select === 'pepperoni'){
-	cost += 400;
-}else if (select === 'mushroom'){
-cost += 500;
-}
-if (select === 'bacon'){
-cost += 600;
-}if (select === 'barbeque'){
-cost += 700;
+var flavor = document.getElementById('flavor').value;
+costFlavor = 0;
+if (flavor === 'pepperoni'){
+	costFlavor += 400;
+}else if (flavor === 'mushroom'){
+costFlavor += 500;
+} else if (flavor === 'bacon'){
+costFlavor += 600;
+}else if (flavor === 'barbeque'){
+costFlavor += 700;
 }
 
-/*
-var toppings = document.getElementsByName('tops').value
+var toppings = document.querySelectorAll("input[name=tops]:checked")[0].value
+
   costTops = 0
   if (toppings === 'Vegetable'){
    costTops += 250
@@ -53,10 +63,10 @@ var delivery = document.getElementsByName('del').value
   } else {
     costDel = 0
   }
-//var qty = document.getElementById('qty').value
+var qty = document.getElementById('qty').value
 
-var totalCharge = ((costFlavor + costTops) + costDel);
-alert(costFlavor) */
-
-document.getElementById('price').innerHTML = "Your order total is Ksh. " + select + "."
+var totalCharge = ((costSize + costFlavor + costTops) * qty) + costDel
+alert(totalCharge) 
+var today = new Date()
+document.getElementById('price').innerHTML = today + "Your order total is Ksh. " + totalCharge + "."
 }
